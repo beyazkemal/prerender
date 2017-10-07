5c5becc75a677e11a319c0fe349108dd82839f81
Kemal Beyaz <kemal@chcdesign.com>
2017-10-07 22:30:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
 // server.use(prerender.logger());
-server.use(prerender.removeScriptTags());
+//server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
